a9693f301ed6f0095e6e2b1c4cbdaf9d5230709a
Ghalib <gmohamed@aegon.nl>
2020-09-08 13:45:01 +0200

    adding POJO. Closes #2.

---
+++ src/test/java/mgn/springframework/springpetclinic/model/Pet.java
@@ -0,0 +1,15 @@
+package mgn.springframework.springpetclinic.model;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pet {
+
+  private PetType petType;
+  private Owner owner;
+  private LocalDate birthDate;
+}
