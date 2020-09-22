ebe6d15cea1849501017f03310182e00b4d958bd
Ghalib <gmohamed@aegon.nl>
2020-09-22 13:35:28 +0200

    Added vet list

--- pet-clinic-web/src/main/java/mgn/springframework/springpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/mgn/springframework/springpetclinic/controller/VetController.java
@@ -1,13 +1,23 @@
 package mgn.springframework.springpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import mgn.springframework.springpetclinic.service.VetService;
 
 @Controller
 public class VetController {
 
+  private final VetService vetService;
+
+  public VetController(final VetService vetService) {
+    this.vetService = vetService;
+  }
+
   @GetMapping("/vets")
-  public String listVets() {
+  public String listVets(Model model) {
+    model.addAttribute("vets", vetService.findAll());
     return "vets/index";
   }
 }
