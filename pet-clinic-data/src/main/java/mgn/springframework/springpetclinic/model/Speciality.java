588dac34bae26e005c6fa6c25d80a7215fcbb50b
Ghalib <gmohamed@aegon.nl>
2020-10-12 16:40:11 +0200

    Add Speciality entity

--- pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Speciality.java
@@ -1,12 +1,11 @@
 package mgn.springframework.springpetclinic.model;
 
-import java.util.Set;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Vet extends Person {
-  private Set<Speciality> specialities;
+public class Speciality extends BaseEntity {
+
+  private String description;
 }
