91b5e6cb729f60e489456309f0e36a0f40a3a58a
Ghalib <gmohamed@aegon.nl>
2020-10-12 16:35:34 +0200

    Add Visit entity

--- pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Owner.java
@@ -1,4 +1,13 @@
 package mgn.springframework.springpetclinic.model;
 
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Owner extends Person {
+
+  private Set<Pet> pets;
 }
---
+++ pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Visit.java
@@ -0,0 +1,15 @@
+package mgn.springframework.springpetclinic.model;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Visit extends BaseEntity {
+
+  private LocalDate date;
+  private String description;
+  private Pet pet;
+}
