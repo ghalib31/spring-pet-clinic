eaf52d514231fe3cd79b0edfaa363f7d970f9781
Ghalib <gmohamed@aegon.nl>
2021-01-27 16:47:15 +0100

    Updated vet with JPA annotations

--- pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Speciality.java
@@ -1,11 +1,18 @@
 package mgn.springframework.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "specialities")
 @Getter
 @Setter
 public class Speciality extends BaseEntity {
 
+  @Column(name = "description")
   private String description;
 }
--- pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mgn/springframework/springpetclinic/model/Vet.java
@@ -3,11 +3,23 @@ package mgn.springframework.springpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "vets")
 @Getter
 @Setter
 public class Vet extends Person {
+  @ManyToMany(fetch = FetchType.EAGER)
+  @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+      inverseJoinColumns = @JoinColumn(name = "speciality_id"))
   private Set<Speciality> specialities = new HashSet<>();
 }
